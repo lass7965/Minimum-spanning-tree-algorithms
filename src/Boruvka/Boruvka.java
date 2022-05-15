a5762a1849577cddc7e0d062acd2e5ac353d96c5
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2022-05-16 01:54:06 +0200

    9 Integrating weights

--- src/Boruvka/Boruvka.java
+++ src/Boruvka/Boruvka.java
@@ -21,17 +21,16 @@ public class Boruvka {
         String[] line1 = fileReader.nextLine().split(",");
         orgN = n = Integer.parseInt(line1[0]);
         m = Integer.parseInt(line1[1]);
-        orgEdges = new int[m * 3]; // [Endpoint1, Endpoint2, Weight,...]
-        weights = new float[m];
+        orgEdges = new int[m * 4]; // [Endpoint1, Endpoint2, edgeID, WeightAsInt,...]
         int nextWrite = 0;
-        int nextWeight = 0;
+        int edgeID = 0;
         while (fileReader.hasNext()) {
             String line = fileReader.nextLine();
             String[] values = line.split(",");
             orgEdges[nextWrite++] = Integer.parseInt(values[0]);
             orgEdges[nextWrite++] = Integer.parseInt(values[1]);
-            orgEdges[nextWrite++] = nextWeight;
-            weights[nextWeight++] = Float.parseFloat(values[2]);
+            orgEdges[nextWrite++] = edgeID++;
+            orgEdges[nextWrite++] = Float.floatToIntBits(Float.parseFloat(values[2]));
         }
         /**                         File reader                          **/
         /**                         Start timer                          **/
@@ -41,12 +40,12 @@ public class Boruvka {
         ArrayList MST = new ArrayList();
         int[] edges = Arrays.copyOf(orgEdges,orgEdges.length);
         while(edges.length > 1){
-            int[] markedEdges = findCheapest(edges,weights,MST,n);
+            int[] markedEdges = findCheapest(edges,MST,n);
             int[] translateTable = UnionFind.getCC(markedEdges,n);
-            edges = contract(edges,weights,translateTable);
+            edges = contract(edges,translateTable);
             n = edges[edges.length-1];
         }
-        int[] ret = getMST(orgEdges,weights,orgN,MST);
+        int[] ret = getMST(orgEdges,orgN,MST);
         /**                         End timer                          **/
         profilerMain.labels.add("Boruvka");
         profilerMain.profiling.add(System.nanoTime());
@@ -55,42 +54,46 @@ public class Boruvka {
     }
 
 
-    public static int[] findCheapest(int[] edges, float[] weights, ArrayList MST, int n){
-        int[] candidateList = new int[n*2]; //[edgeID, endpoint2]
-        for (int i = 0; i < edges.length-1; i+=3) {
-            int edgeID = edges[i+2];
+    public static int[] findCheapest(int[] edges, ArrayList MST, int n){
+        int[] candidateList = new int[n*3]; //[edgeID, edgeWeight, endpoint2]
+        for (int i = 0; i < n; i+=1) {
+            candidateList[i*3+1] = Integer.MAX_VALUE;
+        }
+        for (int i = 0; i < edges.length-1; i+=4) {
             int endpoint1 = edges[i];
             int endpoint2 = edges[i+1];
-            if (isCheaper(candidateList,weights,endpoint1,edgeID)){
-                candidateList[endpoint1*2] = edgeID+1;
-                candidateList[endpoint1*2+1] = endpoint2;
-            }
-            if (isCheaper(candidateList,weights,endpoint2,edgeID)){
-                candidateList[endpoint2*2] = edgeID+1;
-                candidateList[endpoint2*2+1] = endpoint1;
-            }
+            int edgeID = edges[i+2];
+            int edgeWeight = edges[i+3];
+            checkWeight(candidateList,edgeWeight,edgeID,endpoint1,endpoint2);
+            checkWeight(candidateList,edgeWeight,edgeID,endpoint2,endpoint1);
         }
-        for (int i = 0; i < candidateList.length; i+=2) {
-            int edgeID = candidateList[i] - 1;
-            if (edgeID == -1) continue;
-            MST.add(edgeID);
-            candidateList[i] = i / 2;
-        }
-        return candidateList;
+        return getMarked(MST,n,candidateList);
     }
 
-    public static boolean isCheaper(int[] candidateList, float[] weights, int target, int edgeID){
-        int candidateEdgeID = candidateList[target*2];
-        if (candidateEdgeID == 0) return true; //Check if it's the first edge candidate.
-        return weights[candidateEdgeID-1] > weights[edgeID];
+    public static void checkWeight(int[] candidateList, int edgeWeight, int edgeID, int source, int target){
+        if(edgeWeight < candidateList[source*3+1]){
+            candidateList[source*3] = edgeID;
+            candidateList[source*3+1] = edgeWeight;
+            candidateList[source*3+2] = target;
+        }
     }
 
+    public static int[] getMarked(ArrayList MST, int n, int[] candidateList){
+        int[] markedEdges = new int[n*2];
+        int nextWrite = 0;
+        for (int i = 0; i < candidateList.length; i+=3) {
+            if(candidateList[i+1] == Integer.MAX_VALUE) continue;
+            MST.add(candidateList[i]);
+            markedEdges[nextWrite++] = i/3;
+            markedEdges[nextWrite++] = candidateList[i+2];
+        }
+        return markedEdges;
+    }
 
-
-    public static int[] contract(int[] edges, float[] weights, int[] translateTable){
+    public static int[] contract(int[] edges, int[] translateTable){
         int nextWrite = 0;
         int[] sortKeys = {1,0};
-        for (int i = 0; i < edges.length-1; i+=3) {
+        for (int i = 0; i < edges.length-1; i+=4) {
             int h_i = -translateTable[edges[i]]-1;
             int h_j = -translateTable[edges[i+1]]-1;
             if (h_i == h_j) continue;
@@ -102,27 +105,27 @@ public class Boruvka {
                 edges[nextWrite++] = h_i;
             }
             edges[nextWrite++] = edges[i+2];
+            edges[nextWrite++] = edges[i+3];
         }
         if(nextWrite == 0) return new int[1];
-        return Sort.radixSort(edges, weights,sortKeys,translateTable[translateTable.length-1],nextWrite);
+        return Sort.radixSort(edges,sortKeys,translateTable[translateTable.length-1],nextWrite);
     }
 
 
-    public static int[] getMST(int[] edges,float[] weights, int n, ArrayList MST){
+    public static int[] getMST(int[] edges, int n, ArrayList MST){
         int[] mstEdges = new int[(n-1)*3];
         int nextWrite = 0;
         for (int i = 0; i < MST.size(); i++) {
             int edgeID = MST.get(i);
-            float weight = weights[edgeID];
+            int weight = edges[edgeID*4+3];
             if(weight >= 0) {
                 mstEdges[nextWrite++] = edges[edgeID * 2];
                 mstEdges[nextWrite++] = edges[edgeID * 2 + 1];
-                mstEdges[nextWrite++] = Float.floatToIntBits(weight);
-                weights[edgeID] = -weight;
+                mstEdges[nextWrite++] = weight;
+                edges[edgeID*4+3] = -weight;
             }
         }
         if(nextWrite != (n-1)*3) mstEdges = Arrays.copyOf(mstEdges,nextWrite);
-
         return mstEdges;
     }
 }
--- src/Boruvka/Sort.java
+++ src/Boruvka/Sort.java
@@ -2,60 +2,64 @@ package Boruvka;
 
 public class Sort {
     //Preform counting sort per index defined in sortKeys. First index in sortKeys is the "highest priority" of the sorting.
-    public static int[] radixSort(int[] pileOfEdges, float[] weights, int[] sortKeys,int maxID, int size){
+    public static int[] radixSort(int[] pileOfEdges, int[] sortKeys,int maxID, int size){
         for (int key: sortKeys) {
             pileOfEdges = countingSortReversed(pileOfEdges,key,maxID,size);
         }
-        return removeDuplicates(pileOfEdges,weights, maxID);
+        return removeDuplicates(pileOfEdges, maxID);
     }
 
     // CountingSort but reversed in the last for loop. Normally it's from size to 1. Psuedocode in Cormen Introduction to algorithms page 195
     public static int[] countingSortReversed(int[] pileOfEdges, int sortKey,int maxID,int size){
         int[] count = new int[maxID];
         int[] result = new int[size];
-        for (int i = sortKey; i < size; i+=3) {
+        for (int i = sortKey; i < size; i+=4) {
             int id = pileOfEdges[i];
             count[id]++;
         }
         for (int i = 1; i < maxID; i++) {
             count[i] += count[i-1];
         }
-        for (int i = 0; i < size; i+=3) {
+        for (int i = 0; i < size; i+=4) {
             int id = pileOfEdges[i+sortKey];
-            int newIndex = --count[id]*3;
+            int newIndex = --count[id]*4;
             result[newIndex++] = pileOfEdges[i];
             result[newIndex++] = pileOfEdges[i+1];
-            result[newIndex] = pileOfEdges[i+2];
+            result[newIndex++] = pileOfEdges[i+2];
+            result[newIndex] = pileOfEdges[i+3];
         }
         return result;
     }
 
 
-    public static int[] removeDuplicates(int[] pileOfEdges, float[] weights, int n){
+    public static int[] removeDuplicates(int[] pileOfEdges, int n){
         ArrayList edges = new ArrayList();
         int endpoint1 = pileOfEdges[0];
         int endpoint2 = pileOfEdges[1];
         int edgeID = pileOfEdges[2];
-        for (int i = 3; i < pileOfEdges.length; i+=3) {
+        int weight = pileOfEdges[3];
+        for (int i = 4; i < pileOfEdges.length; i+=4) {
             if(endpoint1 != pileOfEdges[i] || endpoint2 != pileOfEdges[i+1]){
                 edges.add(endpoint1);
                 edges.add(endpoint2);
                 edges.add(edgeID);
+                edges.add(weight);
                 endpoint1 = pileOfEdges[i];
                 endpoint2 = pileOfEdges[i+1];
                 edgeID = pileOfEdges[i+2];
+                weight = pileOfEdges[i+3];
             } else {
-                if(weights[pileOfEdges[i+2]] < weights[edgeID]) {
+                if(pileOfEdges[i+3] < weight) {
                     edgeID = pileOfEdges[i+2];
+                    weight = pileOfEdges[i+3];
                 }
             }
         }
         edges.add(endpoint1);
         edges.add(endpoint2);
         edges.add(edgeID);
+        edges.add(weight);
         edges.add(n);
-        //Profiler.profilerMain.labels.add("06 Contract - Remove duplicates");
-        //Profiler.profilerMain.profiling.add(System.nanoTime());
         return edges.getArray();
     }
 }
