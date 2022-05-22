107a659ed90fdda9c6f4ac8ce1ddc9188711df82
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2022-05-22 15:29:59 +0200

    Update Sort.java

--- src/Boruvka/Sort.java
+++ src/Boruvka/Sort.java
@@ -2,66 +2,67 @@ package Boruvka;
 
 public class Sort {
     //Preform counting sort per index defined in sortKeys. First index in sortKeys is the "highest priority" of the sorting.
-    public static int[] radixSort(int[] pileOfEdges, int[] sortKeys,int maxID, int size){
+    public static Graph radixSort(int[] pileOfEdges, int[] sortKeys,int maxID, int size){
         for (int key: sortKeys) {
             pileOfEdges = countingSortReversed(pileOfEdges,key,maxID,size);
         }
-        return pileOfEdges;
+        return buildGraph(pileOfEdges, maxID);
     }
 
     // CountingSort but reversed in the last for loop. Normally it's from size to 1. Psuedocode in Cormen Introduction to algorithms page 195
     public static int[] countingSortReversed(int[] pileOfEdges, int sortKey,int maxID,int size){
         int[] count = new int[maxID];
-        int[] result = new int[size+1];
-        for (int i = sortKey; i < size; i+=4) {
+        int[] result = new int[size];
+        for (int i = sortKey; i < size; i+=3) {
             int id = pileOfEdges[i];
             count[id]++;
         }
         for (int i = 1; i < maxID; i++) {
             count[i] += count[i-1];
         }
-        for (int i = 0; i < size; i+=4) {
+        for (int i = 0; i < size; i+=3) {
             int id = pileOfEdges[i+sortKey];
-            int newIndex = --count[id]*4;
+            int newIndex = --count[id]*3;
             result[newIndex++] = pileOfEdges[i];
             result[newIndex++] = pileOfEdges[i+1];
-            result[newIndex++] = pileOfEdges[i+2];
-            result[newIndex] = pileOfEdges[i+3];
+            result[newIndex] = pileOfEdges[i+2];
         }
-        result[size] = maxID;
         return result;
     }
 
-
-    public static int[] removeDuplicates(int[] pileOfEdges, int n){
-        ArrayList edges = new ArrayList();
-        int endpoint1 = pileOfEdges[0];
-        int endpoint2 = pileOfEdges[1];
-        int edgeID = pileOfEdges[2];
-        int weight = pileOfEdges[3];
-        for (int i = 4; i < pileOfEdges.length; i+=4) {
-            if(endpoint1 != pileOfEdges[i] || endpoint2 != pileOfEdges[i+1]){
-                edges.add(endpoint1);
-                edges.add(endpoint2);
-                edges.add(edgeID);
-                edges.add(weight);
-                endpoint1 = pileOfEdges[i];
-                endpoint2 = pileOfEdges[i+1];
-                edgeID = pileOfEdges[i+2];
-                weight = pileOfEdges[i+3];
-            } else {
-                if(pileOfEdges[i+3] < weight) {
-                    edgeID = pileOfEdges[i+2];
-                    weight = pileOfEdges[i+3];
+    // Build a graph from the sortedData set given, here edges should be considered directed, even though they are undirect, but the set contains both {i,j} and {j,i}.
+    public static Graph buildGraph(int[] pileOfEdges, int maxID){
+        if(pileOfEdges.length == 0){
+            return new Graph(0);
+        }
+        Graph g = new Graph(maxID);
+        ArrayList edgesForVertex = new ArrayList(); // ArrayList for the current vertex in form {target, edgeID}
+        int currentVertexID = pileOfEdges[0];
+        int currentTarget = pileOfEdges[1];
+        int currentEdgeID = pileOfEdges[2];
+        for (int i = 3; i < pileOfEdges.length; i+=3) {
+            if(currentVertexID == pileOfEdges[i]) { // pileOfEdges still points towards same vertex
+                if(currentTarget == pileOfEdges[i+1]){
+                    if(g.isCheaper(currentEdgeID, pileOfEdges[i+2])) currentEdgeID = pileOfEdges[i+2];
+                } else {
+                    edgesForVertex.add(currentTarget);
+                    edgesForVertex.add(currentEdgeID);
+                    currentTarget = pileOfEdges[i+1];
+                    currentEdgeID = pileOfEdges[i+2];
                 }
+            } else {
+                edgesForVertex.add(currentTarget);
+                edgesForVertex.add(currentEdgeID);
+                g.addVertex(currentVertexID,edgesForVertex);
+                currentVertexID = pileOfEdges[i];
+                currentTarget = pileOfEdges[i+1];
+                currentEdgeID = pileOfEdges[i+2];
             }
         }
-        edges.add(endpoint1);
-        edges.add(endpoint2);
-        edges.add(edgeID);
-        edges.add(weight);
-        edges.add(n);
-        return edges.getArray();
+        edgesForVertex.add(currentTarget);
+        edgesForVertex.add(currentEdgeID);
+        g.addVertex(currentVertexID,edgesForVertex);
+        return g;
     }
 }
 
