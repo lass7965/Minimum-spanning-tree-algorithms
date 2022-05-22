ec1f35aedcb3e415ca7cc7830eb64b225c511930
Lasse Sørensen <33939871+lass7965@users.noreply.github.com>
2022-05-22 05:25:35 +0200

    Prims ready for different versions

--- src/Prims/Prims.java
+++ src/Prims/Prims.java
@@ -12,15 +12,15 @@ public class Prims {
         int n;
         int m;
         int[] edges;
-        // TODO revert this file back
+
         /**                         File reader                          **/
         Scanner fileReader = new Scanner(new File(FilePath));
         String[] line1 = fileReader.nextLine().split(",");
         n = Integer.parseInt(line1[0]);
         m = Integer.parseInt(line1[1]);
-        edges = new int[m*3]; // [Endpoint1, Endpoint2, Weight,...]
+        edges = new int[m * 3]; // [Endpoint1, Endpoint2, Weight,...]
         int nextWrite = 0;
-        while(fileReader.hasNext()){
+        while (fileReader.hasNext()) {
             String line = fileReader.nextLine();
             String[] values = line.split(",");
             edges[nextWrite++] = Integer.parseInt(values[0]);
@@ -34,34 +34,36 @@ public class Prims {
         profilerMain.profiling.add(System.nanoTime());
         /**                         Start timer                          **/
 
-        int root = 0;
+        ArrayList MST = new ArrayList();
         Graph g = new Graph(edges,n);
-        minHeap PQ = new minHeap();
-        int[] weights = new int[n];
-        int[] parents = new int[n];
+        minHeap PQ = new minHeap(n);
         for (int u = 0; u < n; u++) {
             PQ.insert(u,Integer.MAX_VALUE);
         }
-        parents[root] = -1;
-        PQ.decreaseValue(root,0);
         while(PQ.size != 0){
             int[] v = PQ.popMin();
             int VertexID =  v[0];
-            weights[VertexID] = v[1];
-            if(v[1] == Integer.MAX_VALUE){
-                parents[VertexID] = -1; //Mark it as root for MSF.
+            int parent = v[2];
+            if( parent != -1 ) {
+                if(parent > VertexID) {
+                    MST.add(VertexID); // Add vertex
+                    MST.add(parent); // Add parent
+                } else {
+                    MST.add(parent); // Add parent
+                    MST.add(VertexID); // Add vertex
+                }
+                MST.add(v[1]); // Add weight
             }
             int[] Neighbors = g.vertices[VertexID];
             for (int i = 0; i < Neighbors.length;i+=2) {
                 int neighbor = Neighbors[i];
                 int weight = Neighbors[i+1];
-                if(PQ.contains(neighbor) && weight < PQ.getKey(neighbor)){
-                    parents[neighbor] = VertexID;
-                    PQ.decreaseValue(neighbor, weight);
+                if(PQ.contains(neighbor,weight)){
+                    PQ.decreaseValue(neighbor, weight,VertexID);
                 }
             }
         }
-        int[] ret = normalizeOutput(parents,weights);
+        int[] ret = MST.getArray();
 
         /**                         End timer                            **/
         profilerMain.labels.add("Prims");
@@ -69,33 +71,7 @@ public class Prims {
         /**                         End timer                            **/
 
         return ret;
-    }
-
-
-    public static int[] normalizeOutput(int[] parents, int[] weights){
-        int[] MST = new int[3*(parents.length-1)];
-        int nextWrite = 0;
-        for (int i = 0; i < parents.length; i++) {
-            if(parents[i] == -1) continue; //Is root for MST
-            int weight = weights[i];
-            if(weight>=0){
-                int target = parents[i];
-                if(parents[target] == i) {
-                    weights[target] = -weight;
-                }
-                weights[i] = -weight;
-                if( i < target) {
-                    MST[nextWrite++] = i;
-                    MST[nextWrite++] = target;
-                } else {
-                    MST[nextWrite++] = target;
-                    MST[nextWrite++] = i;
-                }
-                MST[nextWrite++] = weight;
-            }
-        }
-        if(nextWrite != (parents.length-1)*3) MST = Arrays.copyOf(MST,nextWrite);
-        return MST;
+        //return parents;
     }
 
     public static void main(String[] args) throws FileNotFoundException {
--- src/Prims/minHeap.java
+++ src/Prims/minHeap.java
@@ -4,35 +4,37 @@ import Boruvka.ArrayList;
 import java.util.HashMap;
 
 public class minHeap {
-    public ArrayList heap; // TODO int[] of size n
-    HashMap<Object, Integer> index; // TODO Make this to a int[] of size n
+    public int[] heap;
+    int[] index;
     int size;
 
 
-    public minHeap(){
-        heap = new ArrayList();
-        index = new HashMap<>();
+    public minHeap(int n){
+        heap = new int[3*n]; // [vertexID, Key, Parent]
+        index = new int[2*n]; // [Index, Key]
         this.size = 0;
     }
 
     public void minHeapify(int i){
-        if (this.heap.size() == 0) return;
+        if (this.heap.length == 0) return;
         int smallest = i;
         int lChild = i*2+1;
         int rChild = i*2+2;
-        if(lChild < this.size && heap.get(lChild*2+1) < heap.get(i*2+1)) smallest = lChild;
-        if(rChild < this.size && heap.get(rChild*2+1) < heap.get(smallest*2+1)) smallest = rChild;
-        if (smallest != i){
+        if(lChild < this.size && heap[lChild*3+1] < heap[i*3+1]) smallest = lChild;
+        if(rChild < this.size && heap[rChild*3+1] < heap[smallest*3+1]) smallest = rChild;
+        if(smallest != i) {
             exchange(i,smallest);
             minHeapify(smallest);
         }
     }
 
-    public void decreaseValue(int id, int weight){
-        int i = index.get(id);
-        heap.set(i*2+1,weight);
+    public void decreaseValue(int id, int weight,int parentID){
+        int i = index[id*2];
+        index[2*id+1] = weight;
+        heap[i*3+1] = weight;
+        heap[i*3+2]  = parentID;
         int parent = (i-1)/2;
-        while( i > 0 &&  heap.get(parent*2+1) > heap.get(i*2+1)){
+        while( i > 0 &&  heap[parent*3+1] > heap[i*3+1]){
             exchange(parent,i);
             i = parent;
             parent = (i-1)/2;
@@ -41,70 +43,49 @@ public class minHeap {
 
     public void insert(int vertexID, int weight){
         int i = this.size++;
-        heap.add(vertexID);
-        heap.add(weight);
-        index.put(vertexID,i);
-
-        int parent = (i-1)/2;
-
-        while(i > 0 && heap.get(parent*2+1) > heap.get(i*2+1) ) {
-            exchange(i,parent);
-            i = parent;
-            parent = (i-1)/2;
-        }
-    }
-
-    public void markTakenOut(int id){
-        this.index.put(id,-1);
+        heap[i*3] = vertexID;
+        heap[i*3+1] = weight;
+        heap[i*3+2] = -1;
+        index[vertexID*2] = i;
+        index[vertexID*2+1] = weight;
     }
 
     public int[] getMin(){
-        return new int[]{heap.get(0),heap.get(1)}; //[vertexID, Weight, Parent]
+        return new int[]{heap[0],heap[1],heap[2]}; //[vertexID, Weight, Parent]
     }
 
     public int getKey(int id){
-        return heap.get(index.get(id)*2+1);
+        return index[id*2+1];
     }
 
     public int[] popMin(){
         if(size == 0) return null;
         int[] ret = getMin();
-        index.remove(ret[0]);
+        index[ret[0]*2] = -1;
         if(size-- > 1) {
-            heap.set(1, heap.remove());
-            heap.set(0, heap.remove());
+            heap[0] = heap[size*3]; // GET vertex ID
+            heap[1] = heap[size*3+1]; // GET vertex ID
+            heap[2] = heap[size*3+2]; // GET vertex ID
+            index[heap[0]*2] = 0;
         }
         minHeapify(0);
         return ret;
     }
 
-    public boolean contains(int id) {
-        return index.get(id) != null;
+    public boolean contains(int id, int weight){
+        return index[2*id] >= 0 && weight < index[2*id+1];
+
     }
 
     public void exchange(int swap1, int swap2){
-        int[] swap1Obj = {heap.get(swap1*2),heap.get(swap1*2+1)};
-        int[] swap2Obj = {heap.get(swap2*2),heap.get(swap2*2+1)};
-        heap.set(swap1*2,heap.get(swap2*2));
-        heap.set(swap1*2+1,heap.get(swap2*2+1));
-        heap.set(swap2*2,swap1Obj[0]);
-        heap.set(swap2*2+1,swap1Obj[1]);
-        index.put(swap1Obj[0],swap2);
-        index.put(swap2Obj[0],swap1);
-    }
-
-    public boolean checkStructure(int i){
-        int lchild = i*2+1;
-        int rchild = i*2+2;
-        if(i > size) return true;
-        if(lchild < size && heap.get(i*2+1) > heap.get(lchild*2+1)) { // Parent bigger than left child
-            System.out.println(i*2+1 + " is bigger than " + (lchild*2+1));
-            return false;
-        } else if (rchild < size && heap.get(i*2+1) > heap.get(rchild*2+1)){
-            System.out.println(i*2+1 + " is bigger than " + (rchild*2+1));
-            return false;
-        } else {
-            return checkStructure(lchild) && checkStructure(rchild);
-        }
+        int[] data = new int[]{heap[swap1*3],heap[swap1*3+1],heap[swap1*3+2]};
+        index[heap[swap1*3]*2] = swap2;
+        index[heap[swap2*3]*2] = swap1;
+        heap[swap1*3] = heap[swap2*3];
+        heap[swap1*3+1] = heap[swap2*3+1];
+        heap[swap1*3+2] = heap[swap2*3+2];
+        heap[swap2*3] = data[0];
+        heap[swap2*3+1] = data[1];
+        heap[swap2*3+2] = data[2];
     }
 }
